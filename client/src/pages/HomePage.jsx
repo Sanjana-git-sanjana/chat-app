76c24b7f1918c320084aea6e78c6f7fc24bf5112
SANJANABHOWMICK <sanjanabhowmick06@gmail.com>
2025-10-19 00:50:35 +0530

    3rd commit

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
-import RightSidebar from '../components/RightSideBar'
+import RightSideBar from '../components/RightSideBar'
 import { ChatContext } from '../../context/ChatContext'
+
 
 const HomePage = () => {
 
@@ -14,7 +15,7 @@ const HomePage = () => {
     <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
         <Sidebar/>
         <ChatContainer />
-        <RightSidebar />
+        <RightSideBar />
      </div>
     </div>
   )
